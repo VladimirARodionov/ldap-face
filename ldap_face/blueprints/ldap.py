212047c12b02ea78528a8ff12ca1acfa6dc871bc
Vladimir Rodionov <varodionov@phoenixit.ru>
2024-11-20 17:09:18 +0300

    add data

--- ldap_face/blueprints/ldap.py
+++ ldap_face/blueprints/ldap.py
@@ -1,5 +1,5 @@
 import ldap3
-from flask import Blueprint, request, jsonify, session
+from flask import Blueprint, request, jsonify
 from flask_cors import cross_origin
 from flask_jwt_extended import jwt_required, get_jwt_identity
 
@@ -31,7 +31,9 @@ def ldap():
             tree = []
             base = {'key': search_base, 'label': search_base, 'data': search_base, 'children': []}
             for resp in conn.response:
-                child = {'key': resp['dn'], 'label': resp['dn'], 'children': []}
+                data = resp['attributes']
+                data.pop('userPassword')
+                child = {'key': resp['dn'], 'label': resp['dn'], 'data': dict(data), 'children': []}
                 base['children'].append(child)
             tree.append(base)
             print()
